b743961f649330ea5a87ffa8a9859d8ff48c1c85
abhishek y <75441358+abyii@users.noreply.github.com>
2024-12-22 13:07:02 +0530

    update

--- src/plugin/controller.ts
+++ src/plugin/controller.ts
@@ -9,7 +9,7 @@ type PluginMessage = {
   paletteName: string;
 };
 
-figma.ui.onmessage = async (msg: PluginMessage) => {
+figma.ui.onmessage = (msg: PluginMessage) => {
   if (msg.type === 'add-to-figma') {
     const frame = figma.createFrame();
     frame.layoutMode = 'HORIZONTAL';
@@ -21,10 +21,10 @@ figma.ui.onmessage = async (msg: PluginMessage) => {
     if (msg.createVars) {
       collection = figma.variables.createVariableCollection(`${msg?.paletteName}-colors`);
     }
-    msg?.colors.forEach(async (_, i) => {
+    msg?.colors.forEach((_, i) => {
       const rect = figma.createRectangle();
       if (msg.createVars) {
-        const colorVariable = figma.variables.createVariable(`${msg?.paletteName}-${i}`, collection?.id, 'COLOR');
+        const colorVariable = figma.variables.createVariable(`${msg?.paletteName}-${i}`, collection, 'COLOR');
         colorVariable.setValueForMode(collection.modes[0].modeId, figma.util.solidPaint(msg?.colors?.[i]).color);
         rect.fills = [figma.variables.setBoundVariableForPaint(rect.fills[0], 'color', colorVariable)];
       } else {
